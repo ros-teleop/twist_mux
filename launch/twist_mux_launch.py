8e6cdb86af97cd5ed2d1a87e8262432b86c61ab8
Victor Lopez <victor.lopez@pal-robotics.com>
2021-02-12 15:34:20 +0100

    Add LaunchArguments
    Signed-off-by: Victor Lopez <victor.lopez@pal-robotics.com>

--- launch/twist_mux_launch.py
+++ launch/twist_mux_launch.py
@@ -22,26 +22,50 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-  config_dir = LaunchConfiguration('config_dir', 
-    default=os.path.join(get_package_share_directory('twist_mux'), 
-    'config', 'twist_mux_topics.yaml'))
+    default_config_locks = os.path.join(get_package_share_directory('twist_mux'),
+                                        'config', 'twist_mux_locks.yaml')
+    default_config_topics = os.path.join(get_package_share_directory('twist_mux'),
+                                         'config', 'twist_mux_topics.yaml')
+    default_config_joystick = os.path.join(get_package_share_directory('twist_mux'),
+                                           'config', 'joystick.yaml')
 
-  return LaunchDescription([
-    Node(
-      package='twist_mux', 
-      node_executable='twist_mux', 
-      output='screen',
-      remappings={('/cmd_vel_out', '/twist_mux/cmd_vel')},
-      parameters=[config_dir]),
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'config_locks',
+            default_value=default_config_locks,
+            description='Default locks config file'),
+        DeclareLaunchArgument(
+            'config_topics',
+            default_value=default_config_topics,
+            description='Default topics config file'),
+        DeclareLaunchArgument(
+            'config_joy',
+            default_value=default_config_joystick,
+            description='Default joystick config file'),
+        DeclareLaunchArgument(
+            'cmd_vel_out',
+            default_value='twist_mux/cmd_vel',
+            description='cmd vel output topic'),
+        Node(
+            package='twist_mux',
+            executable='twist_mux',
+            output='screen',
+            remappings={('/cmd_vel_out', LaunchConfiguration('cmd_vel_out'))},
+            parameters=[
+                LaunchConfiguration('config_locks'),
+                LaunchConfiguration('config_topics'),
+                LaunchConfiguration('config_joy')]
+        ),
 
-    Node(
-      package='twist_mux', 
-      node_executable='twist_marker', 
-      output='screen',
-      remappings={('/twist', '/twist_mux/cmd_vel')},
-      parameters=[{
-        'frame_id': 'base_link',
-        'scale': 1.0,
-        'vertical_position': 2.0}])
-])
+        Node(
+            package='twist_mux',
+            executable='twist_marker',
+            output='screen',
+            remappings={('/twist', LaunchConfiguration('cmd_vel_out'))},
+            parameters=[{
+                'frame_id': 'base_link',
+                'scale': 1.0,
+                'vertical_position': 2.0}])
+            ])
